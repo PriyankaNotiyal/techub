3fe189b6f33a15062ddd298d0e0395917fcee989
PriyankaNotiyal <121881160+PriyankaNotiyal@users.noreply.github.com>
2024-03-14 01:49:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -27,22 +27,22 @@
 
 
 // carousel
-// $('.owl-carousel').owlCarousel({
-//     loop: true,
-//     margin: 20,
-//     nav: true,
-//     responsive: {
-//         0: {
-//             items: 1
-//         },
-//         600: {
-//             items: 2
-//         },
-//         1000: {
-//             items: 3
-//         }
-//     }
-// })
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 20,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+})
 // sign
 
 // search
