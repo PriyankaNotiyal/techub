f0de07d8400335aa8177f8a73e1a85b8a71c9591
PriyankaNotiyal <121881160+PriyankaNotiyal@users.noreply.github.com>
2024-03-14 09:00:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,25 @@
 // });
 
 
-
+function showSignIn() {
+        document.getElementById('container').classList.remove('right-panel-active');
+    }
+    function showSignUp() {
+        document.getElementById('container').classList.add('right-panel-active');
+    }
+        const signUpButton = document.getElementById('signUp');
+        const signInButton = document.getElementById('signIn');
+        const container = document.getElementById('container');
+        const user = document.getElementById('user');
+    
+        user.addEventListener('click', () =>
+        container.classList.remove('right-panel-active'));
+    
+        signUpButton.addEventListener('click', () =>
+        container.classList.add('right-panel-active'));
+    
+        signInButton.addEventListener('click', () =>
+        container.classList.remove('right-panel-active'));
 // carousel
 $('.owl-carousel').owlCarousel({
     loop: true,
